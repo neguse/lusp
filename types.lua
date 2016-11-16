85eabe8041755339d56fa091cf638ec3837333be
neguse <negcee@gmail.com>
2016-11-17 00:02:53 +0900

    implement types

---
+++ types.lua
@@ -0,0 +1,43 @@
+
+-- Implementation of Symbol
+
+Symbol = {}
+Symbol.__index = Symbol
+
+function Symbol.new(s)
+	return setmetatable({s}, Symbol)
+end
+
+function Symbol:__tostring()
+	return self[1]
+end
+
+function Symbol:__eq(o)
+	return self[1] == o[1]
+end
+
+-- Implementation of Cons
+
+Cons = {}
+Cons.__index = Cons
+
+function Cons.new(car, cdr)
+	return setmetatable({car, cdr}, Cons)
+end
+
+function Cons:car()
+	return self[1]
+end
+
+function Cons:cdr()
+	return self[2]
+end
+
+function Cons:set_car(a)
+	self[1] = a
+end
+
+function Cons:set_cdr(d)
+	self[2] = d
+end
+
